59364ab784cdc11394c05a5787a0d93db175b18a
AndeMaxR <102634836+AndeMaxR@users.noreply.github.com>
2023-04-27 14:35:50 -0400

    Add files via upload

--- MarketServer.java
+++ MarketServer.java
@@ -1,8 +1,29 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
 public class MarketServer {
+
+    public static ArrayList<Store> storeMasterArrayList;
+
     public static void main(String[] args) throws IOException {
+        storeMasterArrayList = new ArrayList<>();
+        File storeMasterList = new File("StoreMasterList.txt");
+        if (storeMasterList.exists()) {
+            String content;
+            BufferedReader bfr = new BufferedReader(new FileReader(storeMasterList));
+            while (true) {
+                content = bfr.readLine();
+                if (content == null) {
+                    break;
+                } else {
+                    String owner = content.substring(0, content.indexOf("_"));
+                    String storeName = content.substring(content.indexOf("_") + 1, content.indexOf("."));
+                    storeMasterArrayList.add(new Store(owner, storeName));
+                }
+            }
+        }
+
         ServerSocket serverSocket = new ServerSocket(4242);
         while (true) {
             Socket socket = null;
@@ -10,15 +31,12 @@ public class MarketServer {
                 socket = serverSocket.accept();
                 PrintWriter pw = new PrintWriter(socket.getOutputStream());
                 BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                Thread thread = new ClientHandler(pw, br, socket);
+                Thread thread = new ClientHandler(pw, br, socket, storeMasterArrayList);
                 thread.start();
             } catch (Exception e) {
-                socket.close();
                 e.printStackTrace();
+                socket.close();
             }
-        }
-    }
-}
         }
     }
 }
