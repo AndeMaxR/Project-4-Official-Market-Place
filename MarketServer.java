4355a492037175f49878f837951f86397f8c5070
Aarnav <90441784+AarnavBomma@users.noreply.github.com>
2023-04-28 17:48:04 -0400

    Update MarketServer.java

--- MarketServer.java
+++ MarketServer.java
@@ -1,29 +1,8 @@
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
 
 public class MarketServer {
-
-    public static ArrayList<Store> storeMasterArrayList;
-
     public static void main(String[] args) throws IOException {
-        storeMasterArrayList = new ArrayList<>();
-        File storeMasterList = new File("StoreMasterList.txt");
-        if (storeMasterList.exists()) {
-            String content;
-            BufferedReader bfr = new BufferedReader(new FileReader(storeMasterList));
-            while (true) {
-                content = bfr.readLine();
-                if (content == null) {
-                    break;
-                } else {
-                    String owner = content.substring(0, content.indexOf("_"));
-                    String storeName = content.substring(content.indexOf("_") + 1, content.indexOf("."));
-                    storeMasterArrayList.add(new Store(owner, storeName));
-                }
-            }
-        }
-
         ServerSocket serverSocket = new ServerSocket(4242);
         while (true) {
             Socket socket = null;
@@ -31,11 +10,11 @@ public class MarketServer {
                 socket = serverSocket.accept();
                 PrintWriter pw = new PrintWriter(socket.getOutputStream());
                 BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                Thread thread = new ClientHandler(pw, br, socket, storeMasterArrayList);
+                Thread thread = new ClientHandler(pw, br, socket);
                 thread.start();
             } catch (Exception e) {
-                e.printStackTrace();
                 socket.close();
+                e.printStackTrace();
             }
         }
     }
